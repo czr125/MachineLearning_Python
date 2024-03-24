563164082083e8404b09211bdad2e29d49f38de3
czr_dev <cezar.holanda@outlook.com>
2024-03-24 16:07:28 -0300

    Updated csv refined

--- normalizer.py
+++ normalizer.py
@@ -3,13 +3,22 @@ import matplotlib.pyplot as plt
 
 arquivo = 'imdb_movies.csv'
 
-df = pd.read_csv(arquivo)
-
-top_10_notas = df.nlargest(10, 'score')
-
-plt.plot(df['names'], df['score'])  # Substitua 'coluna_x' e 'coluna_y' pelos nomes das colunas que você deseja plotar
-plt.xlabel('Nomes')  # Substitua 'Rótulo do Eixo X' pelo rótulo adequado para o eixo x
-plt.ylabel('Notas')  # Substitua 'Rótulo do Eixo Y' pelo rótulo adequado para o eixo y
-plt.title('Gráfico de filmes com maiores notas')  # Substitua 'Gráfico de Linha' pelo título do gráfico
-plt.grid(True)  # Adiciona grade ao gráfico (opcional)
-plt.show()  # Mostra o gráfico
+conteudo = pd.read_csv(arquivo)
+df = pd.DataFrame(conteudo)
+df = df.drop('overview', axis=1)
+df = df.drop('revenue', axis=1)
+df = df.drop('status', axis=1)
+df = df.drop('orig_title', axis=1)
+df = df.drop('crew', axis=1)
+filme_maior_orçamento_ind = df['budget_x'].idxmax()
+filme_mais_votado_ind = df['score'].idxmax()
+filme_mais_votado= df.loc[filme_mais_votado_ind, 'names']
+nome_filme_maior_orcamento = df.loc[filme_maior_orçamento_ind, 'names']
+orcamento_filme_maior_orcamento = df.loc[filme_maior_orçamento_ind, 'budget_x']
+df.to_csv('movies_refined.csv', index=False)
+print(df)
+print('O filme mais votado é:', filme_mais_votado)
+print("O filme com o maior orçamento é:", nome_filme_maior_orcamento)
+print("Valor do orçamento:", orcamento_filme_maior_orcamento)
+df.plot()
+plt.show()
